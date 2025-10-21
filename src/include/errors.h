bc631f30d0fc55e7af37295587572578b7f70d99
HitotsumeGod <189803705+HitotsumeGod@users.noreply.github.com>
2025-10-21 14:25:57 -0400

    new format for errep

--- src/include/errors.h
+++ src/include/errors.h
@@ -7,22 +7,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define ERRMSGMAX       2400
 #define FUNCNAMEMAX     40
 #define ERREP_ERR       "ERREP() failed; system memory critical"
 
-#define ERREP(rep, func, err)                                         \
+#define ERREP(rep, func, err, msg)                                    \
         if ((rep = malloc(sizeof(struct errep))) != NULL) {           \
                 rep -> caller = func;                                 \
-                rep -> msg = err;                                     \
+                if (err) {                                            \
+                        rep -> is_error = true;                       \
+                        rep -> report.errmsg = msg;                   \
+                } else {                                               \
+                        rep -> is_error = false;                      \
+                        rep -> report.res = msg;                      \
+                }                                                     \
                 rep -> next = NULL;                                   \
         }
 
 struct errep {
-        char *caller;
-        char *msg;
-        struct errep *next;
+        char                 *caller;
+        bool                 is_error;
+        union {
+                char                *errmsg;
+                byte                res;
+        } report;
+        struct errep         *next;
 };
 
 extern char *ptools_format_errors(struct errep *linked_list);
